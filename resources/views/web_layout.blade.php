dd23dbb2d4c1e9c9582cbdb1118f6aed4dbdd515
mahdimarino <mahdismahi.13@gmail.com>
2025-01-28 11:05:38 +0400

    fix links

--- resources/views/web_layout.blade.php
+++ resources/views/web_layout.blade.php
@@ -21,7 +21,7 @@
         </div>
         <div class="menu-links">
             <ul>
-                <li><a href="home">Home</a></li>
+                <li><a href="/">Home</a></li>
                 <li><a href="about">About</a></li>
                 {{-- <li><a href="work">Work</a></li>
                 <li><a href="blog">Blog</a></li> --}}
